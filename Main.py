ba00176ba1c0c9ed27af0c35e211f1890032423c
Oliver-Attoe <oliverdavidattoe@gmail.com>
2026-07-07 16:15:16 +0100

    Retical Added
    Sprite class used to de-clutter code, aiming line now able to be used.

--- Main.py
+++ Main.py
@@ -2,9 +2,47 @@ import pygame
 from sys import exit
 import math
 
+
+x = 500
+y = 575
+class Player(pygame.sprite.Sprite):
+    
+
+    
+    def __init__(self, x, y):
+        super().__init__()
+        self.original_image = pygame.image.load("Images/GUNN.png").convert_alpha()
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=(x, y))
+        self.barrel_offset = pygame.Vector2(25,12)
+
+
+
+    def follow_mouse(self,x ,y):
+        m_pos = pygame.mouse.get_pos()
+
+
+        x_dist = m_pos[0] - x
+        y_dist = -(m_pos[1] - y)
+
+        self.angle = math.degrees(math.atan2(y_dist, x_dist))
+        self.image = pygame.transform.rotate(self.original_image, self.angle - 180)
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def retical_line(self):
+        barrel_pos = self.barrel_offset.rotate(-self.angle)
+        barrel_pos += self.rect.center
+
+        pygame.draw.line(screen, "white", barrel_pos, pygame.mouse.get_pos(), 3)
+
+
+
 pygame.init()
 screen = pygame.display.set_mode((1000, 800))
 clock = pygame.time.Clock()
+
+player = pygame.sprite.GroupSingle()
+player.add(Player(x, y))
 
 #This section is displayed in start game state
 game_state = "start_menu"
@@ -19,9 +57,7 @@ start_button_rect = start_button.get_rect(center = (500,400))
 ## p = player, bg = background
 bg_surface = pygame.image.load("Images/Background_1.png").convert()
 floor_surface = pygame.image.load("Images/Floor.png").convert()
-p_surface = pygame.image.load("Images/Gun_final.png").convert_alpha() #Chnage to alpha later
-x = 500
-y = 575
+
 
 #Game loop
 while True:
@@ -42,23 +78,13 @@ while True:
         screen.blit(s_text, s_text_rect)
 
     elif game_state == "playing":
-        pos = pygame.mouse.get_pos()
-        x_dist = pos[0] - x
-        y_dist = -(pos[1] - y)
-        angle = math.degrees(math.atan2(y_dist, x_dist))
-
-        p_rotate = pygame.transform.rotate(p_surface, angle - 90)
-        p_rect = p_rotate.get_rect(center = (x, y))
 
         screen.blit(bg_surface, (0,0))
-        screen.blit(floor_surface, (0, 725))
-        screen.blit(p_rotate, p_rect)
-        
-        if p_rect.bottom >= 725: p_rect.bottom = 725
-
-        
-
-
+        screen.blit(floor_surface,(0,725))
+        player.sprite.follow_mouse(x, y)
+        player.draw(screen)
+        player.sprite.retical_line()
+  
 
     pygame.display.update()
     clock.tick(60)
